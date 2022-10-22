fda537059b1864a88deaed5b12d067632cff9db7
justbenjie <filippoknest3@gmail.com>
2022-10-23 01:44:40 +0300

    added init

--- bot/handlers/handlers.py
+++ bot/handlers/handlers.py
@@ -1,7 +1,7 @@
 from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from bot.markup import *
+from ..markup import *
 from aiogram.dispatcher import FSMContext
 from db.models import BookHub
 from sqlalchemy import select, and_
